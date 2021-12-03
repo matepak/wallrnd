6532a6806c9c2316eea2f66185616ac2405ad9c4
Mateusz Szczupak <mateusz.szczupak@hotmail.com>
2021-12-03 22:50:04 +0100

    remove option name

--- Options.cs
+++ Options.cs
@@ -6,7 +6,7 @@ namespace randw
         [Option(Required = true, HelpText = "Set path to folder conatining images")]
         public string Path { get; set; }
 
-        [Option('r', "recursive", Required = false, HelpText = "Access subfolders")] public bool Recursive { get; set; }
+        [Option(Required = false, HelpText = "Access subfolders")] public bool Recursive { get; set; }
         [Option(Default = false, HelpText = "Set random wallpaper from given path")] public bool Random { get; set; }
 
         [Option(Default = false, HelpText = "Set wallpaper centered")] public bool Center { get; set; }
